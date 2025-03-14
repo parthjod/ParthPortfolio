07a53734437d053a89ff80c9685a5e0535b66193
parthjod <hlomello765@gmail.com>
2025-03-14 12:21:57 +0530

    Fixed issue

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,22 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
 import "./index.css";
-import Home from "./Pages/Home";
-import Projects from "./Pages/Projects";
-import Resume from "./Pages/Resume";
-import Experience from "./Pages/Experience";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/experience" element={<Experience />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
