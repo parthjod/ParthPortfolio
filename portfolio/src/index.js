0558e973321d1fb89ecf0b7205a3b5e7b13443a6
parthjod <hlomello765@gmail.com>
2025-03-14 12:08:52 +0530

    Fixed deploy issue

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./Pages/Home";
+import Projects from "./Pages/Projects";
+import Resume from "./Pages/Resume";
+import Experience from "./Pages/Experience";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/*" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/experience" element={<Experience />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
