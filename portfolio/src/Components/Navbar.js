568e3860852507e4ccfd7e36223446f3b0948c99
parthjod <hlomello765@gmail.com>
2025-03-14 15:46:35 +0530

    Ready to Deploy

--- portfolio/src/Components/Navbar.js
+++ portfolio/src/Components/Navbar.js
@@ -66,10 +66,10 @@ function Navbar() {
 }
 
 const navItems = [
-  { href: "https://parthjainpjportfolio.onrender.com/", icon: HomeIcon, label: "Home" },
-  { href: "https://parthjainpjportfolio.onrender.com/projects", icon: ComputerIcon, label: "Projects" },
-  { href: "https://parthjainpjportfolio.onrender.com/experience", icon: DomainIcon, label: "Experience" },
-  { href: "https://parthjainpjportfolio.onrender.com/resume", icon: DescriptionIcon, label: "Resume" },
+  { href: "/", icon: HomeIcon, label: "Home" },
+  { href: "/projects", icon: ComputerIcon, label: "Projects" },
+  { href: "/experience", icon: DomainIcon, label: "Experience" },
+  { href: "/resume", icon: DescriptionIcon, label: "Resume" },
 ];
 
 export default Navbar;
